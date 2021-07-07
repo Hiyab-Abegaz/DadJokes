733264121ba5134500feb7909a0b059d97930340
Hiyab Abegaz <hiyababegaz@gmail>
2021-07-07 15:23:36 -0700

    Added axios

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+
+//Creating event listener for button to genereate jokes
 const button = document.querySelector('.btn')
 button.addEventListener('click', function(event){
-    console.log('Ha Ha Ha')
-});
-
-
+    //Call specific funtions I want to happen after clicking
+    console.log('Ha Ha Ha');
+    getDadJoke();
+})
+const getDadJoke =  async () => {
+    const data = { Headers: { Accept: 'application/json'} }
+    const res = await axios.get('https://icanhazdadjoke.com/', data)
+    console.log(res)
+}

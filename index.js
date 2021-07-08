ae2157603415c0d114bb9ca724058c9f05cc6fc9
Hiyab Abegaz <hiyababegaz@gmail>
2021-07-08 14:20:59 -0700

    Made the joke genorator to fully work

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 
 //Creating event listener for button to genereate jokes
+// connects to the h1 in html
+const jokes = document.getElementById('joke')
 const button = document.querySelector('.btn')
 button.addEventListener('click', function(event){
     //Call specific funtions I want to happen after clicking
-    console.log('Ha Ha Ha');
     getDadJoke();
 })
+
+//Reterive Joke 
+//Set joke to screen
 const getDadJoke =  async () => {
-    const data = { Headers: { Accept: 'application/json'} }
-    const res = await axios.get('https://icanhazdadjoke.com/', data)
-    console.log(res)
+    const tellDadJokesUrl = 'https://icanhazdadjoke.com/';
+        let response;
+        try {
+            response = await axios.get(tellDadJokesUrl, { headers: { "Accept": "text/plain" } });            
+        } catch (error) {
+            response =  "Sorry son, an error has occurred!";
+            return 
+        }
+        const joke = response.data
+        console.log(joke)
+// connecting the h1 with the joke genorater so know it prints when button is clicked
+        jokes.textContent = joke       
 }
